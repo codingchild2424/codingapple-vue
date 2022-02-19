bdf55b96a63661c4714819b594b58d9ef0f61d0c
codingchild <45235027+codingchild2424@users.noreply.github.com>
2022-02-19 18:33:19 +0900

    complete nearly

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
       :popup = "popup" 
       :onerooms = "onerooms" 
       :click = "click" 
-      @closeModal="popup = false" />
+      @closeModal="popup = false"
+    />
   </transition>
-
 
   <div class="menu">
       <a v-for = "baner in baners" :key = "baner">{{ baner }}</a>
@@ -15,7 +15,20 @@
 
   <DiscountComponent/>
 
-  <CardComponent :onerooms="onerooms" :popup="popup" @openModal="popup = true; click=$event"/>
+  <button @click="priceSort()">
+    가격순 정렬
+  </button>
+
+  <button @click="sortBack()">
+    되돌리기
+  </button>
+
+  <CardComponent 
+    :onerooms="onerooms" 
+    :popup="popup" 
+    @openModal="popup = true; 
+    click=$event"
+  />
 
 </template>
 
@@ -31,6 +44,7 @@ export default {
     return {
       baners : ['home', 'menu', 'etc'],
       onerooms : data,
+      original_onerooms : [...data], //사본을 따로 만드는 문법
       popup: false,
       click: 0
     }
@@ -39,6 +53,16 @@ export default {
       DiscountComponent: DiscountComponent,
       ModalComponent: ModalComponent,
       CardComponent: CardComponent,
+  },
+  methods : {
+    priceSort(){
+      this.onerooms.sort(function(a, b){ //그냥 sort를 사용하면, 문자의 순서만 바꿀 수 있음, 따라서 이런 방식으로 사용해야 함
+        return a.price - b.price; //onerooms에 있는 가격 정보만 활용해서 순서를 정하므로
+      })
+    },
+    sortBack(){
+      this.onerooms = [...this.original_onerooms]; //array는 그냥 등호를 쓰면 값을 공유하게 되므로, [...] 문법을 사용하면 됨
+    }
   }
 }
 </script>
